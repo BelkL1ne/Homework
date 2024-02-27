c9c138af319346e7d7b49913d90fd268a33a89f5
Ivan Makhankov <6270103@gmail.com>
2024-02-27 22:16:08 +0300

    commit

---
+++ Homework 8/HomeWork8Interface/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace HomeWork8Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog Dog = new Dog();
+            Dog.SetName();
+            Dog.GetName();
+            Dog.Eat();
+        }
+    }
+    interface Animal
+    {
+        public string Name { get; set; }
+
+        public void GetName();
+
+        public string SetName();
+
+        public void Eat();
+    }
+    class Dog : Animal
+    {
+        string? name;
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public void Eat()
+        {
+            Console.WriteLine($"{name} eat.");
+        }
+        public void GetName()
+        {
+            Console.WriteLine(name);
+        }
+
+        public string SetName()
+        {
+            Console.WriteLine("Name a Dog");
+            name = Console.ReadLine();
+            return name;
+        }
+    }
+}
---
+++ Homework 8/Homework8/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Homework8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog Dog = new Dog();
+            Dog.SetName();
+            Dog.GetName();
+            Dog.Eat();
+        }
+    }
+
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+
+        public abstract void GetName();
+
+        public abstract string SetName();
+
+        public abstract void Eat();
+        
+    }
+    class Dog : Animal
+    {
+        string? name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public override void Eat()
+        {
+            Console.WriteLine ($"{name} eat.");
+        }
+        public override void GetName()
+        {
+            Console.WriteLine(name);
+        }
+
+        public override string SetName()
+        {
+            Console.WriteLine("Name a Dog");
+            name = Console.ReadLine();
+            return name;
+        }
+    }
+}
